bf073645be93b5fe3108114418a842b325f16c1e
Eugene Khristo <marteau9292@gmail.com>
2025-05-12 11:51:04 +0300

    Step1 is ready

---
+++ src/pages/Step1.jsx
@@ -0,0 +1,64 @@
+import { useNavigate } from 'react-router';
+
+function Step1() {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate('/step-2');
+  }
+
+  return (
+    <div className="container step-1">
+      <div className="bg-blur" />
+
+      <header>
+        <div className="top">
+          <img src="/img/step-1/logo.png" alt="logo" />
+          <span>Detected by AI Safe Internet Association</span>
+        </div>
+        <h1>
+          Your connection <br /> is not secure!
+        </h1>
+      </header>
+
+      <main>
+        <img
+          src="/img/step-1/hero.png"
+          alt="man holding mobile phone with security issues elemnts floating around him"
+          className="hero-img"
+        />
+
+        <ul>
+          <li>
+            <span>🕵️</span>
+            <span>Hackers can see your IP</span>
+          </li>
+          <li>
+            <span>🔓</span>
+            <span>Your data isn’t protected</span>
+          </li>
+          <li>
+            <span>🚫</span>
+            <span>No encryption — high risk</span>
+          </li>
+          <li>
+            <span>👁</span>
+            <span>You're being tracked online</span>
+          </li>
+        </ul>
+      </main>
+
+      <footer>
+        <div className="top">
+          <img src="/img/step-1/shield.svg" alt="icon" />
+          <span>Start a free device security check</span>
+        </div>
+        <button className="btn" onClick={handleClick}>
+          Scan your iPhone
+        </button>
+      </footer>
+    </div>
+  );
+}
+
+export default Step1;
