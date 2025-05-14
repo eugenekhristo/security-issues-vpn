5d9e40b6c5c8c6553c638672ae96135a0360dc18
Eugene Khristo <marteau9292@gmail.com>
2025-05-14 16:11:53 +0300

    Step5 layout

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,23 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 import App from './App.jsx';
 
-setTimeout(() => {
-  const el = document.querySelector('.preloader');
-  el.classList.add('hidden');
+// setTimeout(() => {
+//   const el = document.querySelector('.preloader');
+//   el?.classList.add('hidden');
 
-  createRoot(document.getElementById('root')).render(
-    <StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </StrictMode>
-  );
-}, 2000);
+//   createRoot(document.getElementById('root')).render(
+//     <StrictMode>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//     </StrictMode>
+//   );
+// }, 2000);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
--- src/pages/Step5.jsx
+++ src/pages/Step5.jsx
@@ -1,5 +1,47 @@
+import BgBlur from '../ui/BgBlur';
+
 function Step5() {
-  return <div>Step 5</div>;
+  return (
+    <div className="container step-5">
+      <BgBlur backgroundColor={'#007AFF'} />
+
+      <div className="wrapper">
+        <h2>Enter your email</h2>
+
+        <div className="input-box">
+          <div className="input">
+            <label htmlFor="email">
+              Email address <span>(required)</span>
+            </label>
+            <input
+              type="email"
+              required
+              name="email"
+              id="email"
+              placeholder="Enter your email here"
+            />
+          </div>
+          <div className="sub">
+            <img src="/img/step-5/lock.svg" alt="lock-icon" />
+            <p>
+              We respect your privacy and we are committed to protecting your
+              personal data. By continuing you indicate that you’ve read and
+              agree to our{' '}
+              <a href="https://guruvpn.com/terms" target="terms">
+                Terms of Use
+              </a>{' '}
+              and{' '}
+              <a href="https://guruvpn.com/policy" target="privacy">
+                Privacy Policy
+              </a>
+            </p>
+          </div>
+        </div>
+
+        <button className="btn disabled">Continue</button>
+      </div>
+    </div>
+  );
 }
 
 export default Step5;
