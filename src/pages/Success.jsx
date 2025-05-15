84bb199bd5e99a0380f17b16fc83a2b797e4d67e
Eugene Khristo <marteau9292@gmail.com>
2025-05-15 15:55:05 +0300

    Create Layout Success page

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,5 +1,36 @@
+import BgBlur from '../ui/BgBlur';
+
 function Success() {
-  return <div></div>;
+  return (
+    <div className="container success">
+      <BgBlur backgroundColor={'#007AFF'} />
+      <div className="wrapper">
+        <div className="email">
+          <img src="/img/success/check.svg" alt="icon" />
+          <div className="copy_wrapper">
+            <h2>Purchase Confirmed!</h2>
+            <div className="copy">
+              <span className="plain">We've emailed you the download link</span>
+              <div className="user_email">
+                <span className="emoji">📩</span>
+                <span className="text">example@domain.com</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="appstore">
+          <div className="copy">
+            <span className="text">Or simply download it below</span>
+            <span className="emoji">👇</span>
+          </div>
+          <a href="#">
+            <img src="/img/success/appstore.png" alt="download on appstore" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Success;
